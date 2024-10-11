07973422f5225b0f31777aa9b0ebfc818bbce4f0
Your Name <you@example.com>
2024-10-11 13:55:31 +0300

    PZ7_2

--- web_1/hello/WEB1/views.py
+++ web_1/hello/WEB1/views.py
@@ -1,23 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotFound
 from .models import Person
-from django.http import *
-from django.shortcuts import render
+
 def index(request):
-    people = Person.objects.all()
-    return render(request, "index.html", {"people": people})
-def about(request):
-    return HttpResponse("About")
-def contact(request):
-    return HttpResponseRedirect("/about")
-def details(request):
-    return HttpResponsePermanentRedirect("/")
-def product_list(request):
-    products = [
-    {'name': 'Игорь', 'age': 23 },
-    ]
+ people = Person.objects.all()
+ return render(request, "index.html", {"people": people})
 
 def create(request):
  if request.method == "POST":
@@ -27,3 +15,23 @@ def create(request):
     klient.save()
  return HttpResponseRedirect("/")
 
+def edit(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        if request.method == "POST":
+            person.name = request.POST.get("name")
+            person.age = request.POST.get("age")
+            person.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "edit.html", {"person": person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+
+def delete(request, id):
+    try:
+        person = Person.objects.get(id=id)
+        person.delete()
+        return HttpResponseRedirect("/")
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Клиент не найден</h2>")
