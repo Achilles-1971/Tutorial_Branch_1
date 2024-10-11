f2df53d0344d542e9ac01523e512d861fe0d5381
Your Name <you@example.com>
2024-10-11 14:35:47 +0300

    PZ7_4

--- web_1/hello/WEB1/models.py
+++ web_1/hello/WEB1/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-
 class Person(models.Model):
-  name = models.CharField(max_length=20)
-  age = models.IntegerField()
-  objects = models.Manager()
+ name = models.CharField(max_length=20)
+ age = models.IntegerField()
+ objects = models.Manager()
+class Company(models.Model):
+ name = models.CharField(max_length=30)
+class Product(models.Model):
+ company = models.ForeignKey(Company, on_delete=models.CASCADE)
+ name = models.CharField(max_length=30)
+ price = models.IntegerField()
